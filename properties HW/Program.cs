8d83be20bb9d012868942befd3ba7caa14d3cb3c
orgro <orgro@LAPTOP-A99L5IL2>
2022-02-02 01:03:53 +0200

    Add project files.

---
+++ properties HW/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace properties_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quitation Qa = new Quitation();
+            int Numoflines = 5;
+            
+
+           
+                 
+
+           
+           
+        }
+    }
+}
---
+++ properties HW/Quitation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties_HW
+{
+    internal class Quitation
+    {
+        public string CostumerName { get; private set; }
+        public double[] SumLines { get; set; }
+        private double Docsum;
+        public double DocSum
+        {
+            get
+            {
+                for (int i = 0; i < SumLines.Length; i++) ;
+                    return Docsum;
+            }
+        }
+        public DateTime VaildUntil { get; private set; }
+        public Quitation()
+        {
+
+        }
+
+        public Quitation(string costumerName, double[] sumLines, DateTime vaildUntil)
+        {
+            CostumerName = costumerName;
+            costumerName = "or";
+            SumLines = sumLines;
+            VaildUntil = vaildUntil;
+            vaildUntil = DateTime.Now;
+        }
+
+        public void ChangeMainDEtails()
+        {
+            Console.WriteLine("enter your name");
+            CostumerName=Console.ReadLine();
+            VaildUntil=DateTime.Now.AddDays(27);
+        }
+
+
+    }
+}
